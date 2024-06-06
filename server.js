cd8cbeeb1430ef544efcdf3ca15a43fe4cba083d
Kamlesh Patel <kamleshpatel12328@gmail.com>
2024-06-06 19:47:34 +0530

    Added Routing for Vercel Fix 3

--- server.js
+++ server.js
@@ -21,13 +21,10 @@ app.use(
 
 // routes
 app.use(express.static(path.resolve("build")));
-app.get('/login', (req, res) => {
-  res.sendFile(path.resolve('build', 'index.html'));
-});
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/dashboard", requireSignIn, dashboardRoutes);
-app.use("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "build/index.html"));
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve('build', 'index.html'));
 });
 
 app.listen(process.env.PORT, (req, res) => {
